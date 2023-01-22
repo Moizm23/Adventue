ce5b6c2dac71a36db994f04d259a8d041fab0fb5
Moizm23 <97847082+Moizm23@users.noreply.github.com>
2023-01-22 12:39:16 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 import javax.swing.JFrame;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.*;
+
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
@@ -14,7 +20,25 @@ public class Main {
         window. setVisible (true) ;
         
         gamePanel.startGameThread();
-
+        
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\\Users\\USER\\Downloads\\Setups And Extras\\darkmusic.mp3"));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } 
+        catch (LineUnavailableException e) {
+            System.out.println("The audio line is unavailable.");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } 
 
         }
+    	
+
 }
