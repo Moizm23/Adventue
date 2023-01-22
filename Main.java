ee5f70bbb689fb28f41b9eb574e57281241432e3
Moizm23 <97847082+Moizm23@users.noreply.github.com>
2023-01-21 20:40:32 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,17 +2,14 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
+        GamePanel gamePanel = new GamePanel () ;
+        window.add(gamePanel) ;
+        gamePanel.startGameThread();
+
+        window.pack () ;
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ;
         window.setResizable (false) ;
         window.setTitle ("2D Adventure");
-        window. setLocationRelativeTo (null) ;
-        window. setVisible(true) ;
-        
-        GamePanel gamePanel = new GamePanel () ;
-        window.add(gamePanel) ;
-
-        window.pack () ;
-
         window. setLocationRelativeTo (null) ;
         window. setVisible (true) ;
         
